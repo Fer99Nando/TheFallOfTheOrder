d419298e7797dc5790dd21583e61e3b672733186
fernando jimenez cabrera <fer99nandojc@gmail.com>
2018-10-08 17:33:05 +0200

    camara tercera persona video youtube

---
+++ Final Project/Assets/Proyecto Final/Scripts/CameraThirdPerson.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraThirdPerson : MonoBehaviour 
+{
+	public Transform lookAt; //lookAt del personaje
+
+	Vector3 positionIncrease; //Incremento de la posicion de la camara respecto al lookAt
+
+	public float distance; //distancia original de la camara respecto al lookAt
+	float fixedDist; //distancia corregida de la camara respecto al lookAt en caso de collision u oclusion
+	float sensibilityWheel; //sensibilidad del zoom (Rueda del raton)
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.positionIncrease = this.lookAt.forward * this.distance; //Calcula el incremento de la posicion
+		transform.position = this.lookAt.position - this.positionIncrease; //Creamos una posicion orbital
+		transform.LookAt(this.lookAt); // la camara mira al lookAt (puntero)
+
+		this.sensibilityWheel = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.distance += Input.GetAxis("Mouse ScrollWheel") * this.sensibilityWheel; //zoom
+		this.distance = Mathf.Clamp(this.distance, 2f, 6f); // Limitacion de zoom in and zoom out
+
+		this.fixedDist = fixDistance();
+	}
+	void LateUpdate()
+	{
+		this.positionIncrease = this.lookAt.forward * this.fixedDist;
+		transform.position = Vector3.Lerp(transform.position, this.lookAt.position - this.positionIncrease, 25f * Time.deltaTime);
+		transform.LookAt(this.lookAt);
+	}
+	//Corrige la distancia ante las colisiones u oclusiones de la camara
+	float fixDistance()
+	{
+		RaycastHit hit; //Guarda la informacion de la colision
+		LayerMask layerMask = 1 << 8; //Asignamos el layer 8 que es el del player
+		layerMask = ~layerMask; //invierte el layer para que colisione con cualquier layer excepto este
+
+		/*Emite un raycast desde el lookAt a la camara colisionara  */
+
+ 
+	}
+}
