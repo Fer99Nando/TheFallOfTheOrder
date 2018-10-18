b8bf62f513dfb5b0c2fcbaa3f4021720a0cdd214
fernando jimenez cabrera <fer99nandojc@gmail.com>
2018-10-18 19:03:34 +0200

    menu principal con escenas cargado

---
+++ Final Project/Assets/Proyecto Final/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		
+	}
+	public void GameplayScene ()
+	{
+		SceneManager.LoadScene ("Gameplay");
+	}
+	public void OptionsSnece ()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+	public void CreditsScene ()
+	{
+		SceneManager.LoadScene ("Options");
+	}
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
