f5237ddccf5b194f029d6f7a9682f45f1dcc16ee
fernando jimenez cabrera <fernando99jc@gmail.com>
2018-11-24 17:57:13 +0100

    comienzo vida enemigo y la del palyer acabada y virus pero no stamina

---
+++ Final Project/Assets/Proyecto Final/Scripts/IA/EnemyHealth.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHp;
+
+    public int currentHp;
+
+    private int damage;
+
+    public Slider healthSlider;
+
+    public Image damageImage;
+
+    // Sonido muerte
+
+    Animator anim;
+
+    // Sonidos
+
+    EnemyBehaviour playerControl;
+
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerControl = GetComponent<EnemyBehaviour>();
+        currentHp = startingHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        damage = amount;
+        currentHp -= amount;
+
+        healthSlider.value = currentHp;
+
+        // Sonido asignado del jugador
+
+        if (currentHp <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        // Animacion de muerte;
+
+        playerControl.enabled = false;
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Damage"))
+        {
+            damage = 10;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // que se destruya el objeto
+    }
+}
