da50987cc744166afc9497705166d82476f393bc
FernandoJimenezCabrera <fernando99jc@gmail.com>
2018-11-29 19:06:29 +0100

    fade

---
+++ Final Project/Assets/Proyecto Final/Scripts/UI/LogoUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LogoUI : MonoBehaviour 
+{
+		public GameObject fade; 	
+
+	public Animator anim;
+
+	private float timeCounter;
+
+	void Awake()
+	{
+		anim.SetBool("Fade", false);
+	}
+
+	void Update () 
+	{
+		timeCounter += Time.deltaTime;
+		LogoToMenuScene();	
+	}
+
+	public void LogoToMenuScene ()
+	{
+		if (timeCounter >= 7)
+		{
+			SceneManager.LoadScene ("Menu_Principal");
+		}
+
+		if(Input.anyKeyDown && timeCounter <= 7)
+		{
+			StartCoroutine(Fading());
+		}
+	}
+
+	IEnumerator Fading()
+	{
+		anim.SetBool("Fade", true);
+		yield return new WaitForSeconds (1.0f);
+		SceneManager.LoadScene("Menu_Principal");
+	}
+}
--- Final Project/Assets/Proyecto Final/Scripts/UI/UIManager.cs
+++ Final Project/Assets/Proyecto Final/Scripts/UI/UIManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour 
 {
+	public GameObject fade; 	
+
+	public Animator anim;
+
+	private float timeCounter;
+
+	void Awake()
+	{
+		anim.SetBool("Fade", false);
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -14,27 +24,46 @@ public class UIManager : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-
-		
+		timeCounter += Time.deltaTime;
 	}
 	public void MenuPrincipalScene ()
 	{
 		SceneManager.LoadScene ("Menu_Principal");
 	}
+
+	public void ButtonExitToMenu ()
+	{
+		StartCoroutine(Fading());
+		SceneManager.LoadScene ("Menu_Principal");	
+	}
+
 	public void GameplayScene ()
 	{
+		StartCoroutine(Fading());
 		SceneManager.LoadScene ("Gameplay");
 	}
+
 	public void OptionsSnece ()
 	{
+		StartCoroutine(Fading());
 		SceneManager.LoadScene ("Options");
 	}
+
 	public void CreditsScene ()
 	{
+		StartCoroutine(Fading());
 		SceneManager.LoadScene ("Credits");
 	}
+
 	public void Exit()
 	{
 		Application.Quit ();
+	}
+
+	IEnumerator Fading()
+	{
+		anim.SetBool("Fade", true);
+		yield return new WaitForSeconds (1.0f);
+		SceneManager.LoadScene("Menu_Principal");
 	}
 }
