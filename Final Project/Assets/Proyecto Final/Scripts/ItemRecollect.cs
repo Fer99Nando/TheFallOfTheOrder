037bf776e704e282bb16bcb1c7f7b527923b79a2
fernando jimenez cabrera <fer99nandojc@gmail.com>
2019-01-28 17:13:23 +0100

    vida item comenzado

---
+++ Final Project/Assets/Proyecto Final/Scripts/ItemRecollect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemRecollect : MonoBehaviour
+{
+    public GameObject lifePot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifePot.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("Aaa, me tocaste");
+
+            if (lifePot == false)
+            {
+                lifePot.SetActive(true);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Final Project/Assets/Proyecto Final/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    Vector3 rot = new Vector3(0, 180, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rot * Time.deltaTime);
+    }
+}
