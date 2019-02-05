79ea24ab948ce19f3baf9e5208883a9769ec36e7
fernando jimenez cabrera <fernando99jc@gmail.com>
2019-02-05 14:46:30 +0100

    item array probsndo

---
+++ Final Project/Assets/ItemInventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInventory : MonoBehaviour
+{
+    public GameObject [] items;
+
+    bool [] empty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = new GameObject[3];
+
+        items = GameObject.FindGameObjectsWithTag("Item");
+
+        empty = new bool[3];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Inventory()
+    {
+        
+    }
+}
