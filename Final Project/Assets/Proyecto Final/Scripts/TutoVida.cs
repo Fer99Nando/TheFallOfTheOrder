dee58dd9140d07980c2689bb50b6c60f5ae20a0e
fernando jimenez cabrera <fer99nandojc@gmail.com>
2019-05-02 17:13:43 +0200

    ajustando potis del tutorial con otro texto distinto

--- Final Project/Assets/Proyecto Final/Scripts/TutoVida.cs
+++ Final Project/Assets/Proyecto Final/Scripts/TutoVida.cs
@@ -7,21 +7,45 @@ using DG.Tweening;
 public class TutoVida : MonoBehaviour
 {
     public GameObject general;
+
+    public GameObject vida;
+
     public Image general1;
     public Text general2;
+    
+    public GameObject tutAntidoto;
+
+    public GameObject antidoto;
+
+    public Image antidoto1;
+    public Text antidoto2;
+
+    public GameObject tutTotal;
+
+    public GameObject total;
+    
+    public Image total1;
+    public Text total2;
 
     public float timeCount;
     public bool timeOn;
+    public bool timeOnA;
+    public bool timeOnV;
 
     private void Awake()
     {
         timeOn = false;
-        general.SetActive(false);
-    }
+        timeOnA = false;
+        timeOnV = false;
 
+        general.SetActive(false);
+        antidoto.SetActive(false);
+        total.SetActive(false);
+    }
+    
     void Update()
     {
-        if (timeOn)
+        if (timeOn || timeOnA || timeOnV)
         {
             timeCount += Time.deltaTime;
         }
@@ -39,16 +63,50 @@ public class TutoVida : MonoBehaviour
     {
         Destroy(general1);
         Destroy(general2);
+        Destroy(general);
+        Destroy(vida);
+    }
+
+    public void DestroyTitle1()
+    {
+        Destroy(antidoto);
+        Destroy(antidoto1);
+        Destroy(antidoto2);
+        Destroy(tutAntidoto);
+    }
+
+    public void DestroyTitle2()
+    {
+        Destroy(tutTotal);
+        Destroy(total1);
+        Destroy(total2);
+        Destroy(total);
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(vida.tag == "Player")
         {
             Debug.Log("Toco El Collider");
             general.SetActive(true);
             timeOn = true;
-            this.gameObject.SetActive(false);
+            vida.SetActive(false);
+        }
+
+        if (antidoto.tag == "Player")
+        {
+            Debug.Log("Toco El Collider");
+            tutAntidoto.SetActive(true);
+            timeOnA = true;
+            antidoto.SetActive(false);
+        }
+
+        if (total.tag == "Player")
+        {
+            Debug.Log("Toco El Collider");
+            tutTotal.SetActive(true);
+            timeOnV = true;
+            total.SetActive(false);
         }
     }
 }
