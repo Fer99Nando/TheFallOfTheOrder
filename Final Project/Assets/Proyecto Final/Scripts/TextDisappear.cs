24b16f7d7c2d1e2c9150bd165f143b68fea62d90
fernando jimenez cabrera <fer99nandojc@gmail.com>
2019-05-08 19:05:55 +0200

    pensando las primeras pociones y textdisappear

--- Final Project/Assets/Proyecto Final/Scripts/TextDisappear.cs
+++ Final Project/Assets/Proyecto Final/Scripts/TextDisappear.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TextDisappear : MonoBehaviour
 {
+    public float time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+
+        if (time >= 3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
